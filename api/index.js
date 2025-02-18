11325d91b053a9459147570b3dadaf5916d328fb
GabrielCaja <gabrielcajaegido@gmail.com>
2025-02-18 19:15:35 +0100

    Eliminar ruta raíz en el archivo index.js

--- api/index.js
+++ api/index.js
@@ -19,7 +19,6 @@ function getAnimal() {
 }
 
 const app = express();
-app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
